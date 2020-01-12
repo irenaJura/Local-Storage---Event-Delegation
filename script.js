89c1899777b9f6dff03506829dd7f4ad1afe9985
Irena Jurasek <irenaskydive@gmail.com>
2020-01-12 13:38:38 +0100

    add function to add items

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];
+const addItems = document.querySelector('.add-items'); // form
+const itemsList = document.querySelector('.plates'); // ul
+const items = []; // store data in an array of objects, name and done status
+
+// function to add an item and make an object to put in array
+function addItem(e) {
+    e.preventDefault(); // prevent reloading and sending the data 
+    // console.log(e.target);
+    // grab the input box, this is the actual form we're typing in
+    // in case there's more forms on the page
+    const text = (this.querySelector('[name=item]')).value;
+    // take the text from this box and put it in an object
+    // first make an item
+    const item = {
+        text,
+        done: false // not checked by default
+    };
+    //console.log(item);
+    // push item into array
+    items.push(item);
+    console.log(items);
+    this.reset(); // resets input
+}
+
+addItems.addEventListener('submit', addItem); // click the button
+
